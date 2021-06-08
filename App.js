76d270c2458b235711fe983ca35def5a4683ce6a
unknown <vimalkuriensam@gmail.com>
2021-06-08 21:54:22 +0530

    added font and splash screen

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { enableScreens } from "react-native-screens";
 
 import RootNavigator from "./navigation/RootNavigator";
 
+enableScreens();
+
+const fetchFonts = () =>
+  Font.loadAsync({
+    "montserrat-bold": require("./assets/fonts/Montserrat-Bold.otf"),
+    "montserrat-regular": require("./assets/fonts/Montserrat-Regular.otf"),
+    "proximaA-bold": require("./assets/fonts/ProximaNovaAltBold.otf"),
+    "proximaNova-Regular": require("./assets/fonts/ProximaNova-Regular.otf"),
+  });
+
 const App = () => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded)
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+
   return <RootNavigator />;
 };
 
 export default App;
- 
