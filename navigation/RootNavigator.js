b7cddf97cc658f46cf8716d3daced296d2b62de7
unknown <vimalkuriensam@gmail.com>
2021-07-26 00:06:19 +0530

    navigator refactor

--- navigation/RootNavigator.js
+++ navigation/RootNavigator.js
@@ -1,36 +1,8 @@
-// import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import { Auth as AuthScreen } from "../screen";
 
 import ProfileScreen from "../screen/ProfileScreen";
-
-// const tabScreenConfig = {
-//   Info: {
-//     screen: ProfileScreen,
-//   },
-//   Qualification: {
-//     screen: ProfileScreen,
-//   },
-//   Profession: {
-//     screen: ProfileScreen,
-//   },
-//   Skill: {
-//     screen: ProfileScreen,
-//   },
-//   EmployeeDetails: {
-//     screen: ProfileScreen,
-//   },
-//   Overseas: {
-//     screen: ProfileScreen,
-//   },
-//   Documents: {
-//     screen: ProfileScreen,
-//   },
-//   Resume: {
-//     screen: ProfileScreen,
-//   },
-// };
 
 const { Navigation: ProfileNavigation, Screen: ProfilesScreen } =
   createStackNavigator();
@@ -62,22 +34,6 @@ const ProfileDefaultNavOptions = {
   },
 };
 
-// const ProfileNavigation = createStackNavigator(tabScreenConfig, {
-//   defaultNavigationOptions: {
-//     headerTitle: "Create your eshramik resume",
-//     headerStyle: {
-//       backgroundColor: "#FFE1A1",
-//       height: 60,
-//     },
-//     headerTintColor: "#727272",
-//     headerTitleStyle: {
-//       alignSelf: "center",
-//       fontFamily: "poppins-medium",
-//       fontSize: 16,
-//     },
-//   },
-// });
-
 const { Navigator: AuthsNavigator, Screen: AuthsScreen } =
   createStackNavigator();
 
@@ -89,26 +45,3 @@ export const AuthNavigator = () => (
   </AuthsNavigator>
 );
 
-// const AuthNavigator = createStackNavigator(
-//   {
-//     Login: {
-//       screen: AuthScreen,
-//     },
-//     Signup: {
-//       screen: AuthScreen,
-//     },
-//     OTP: {
-//       screen: AuthScreen,
-//     },
-//   },
-//   {
-//     headerMode: false,
-//   }
-// );
-
-// const RootNavigation = createSwitchNavigator({
-//   Profile: ProfileNavigation,
-//   Auth: AuthNavigator,
-// });
-
-// export default createAppContainer(RootNavigation);
